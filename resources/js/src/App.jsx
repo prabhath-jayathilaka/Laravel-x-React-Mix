8d530a295490cf354dbff68bf9e6944426faa379
prabhath-jayathilaka <jayathil_ps17077@stu.kln.ac.lk>
2022-01-27 03:05:11 +0530

    Laravel x React Configuration Successful

---
+++ resources/js/src/App.jsx
@@ -0,0 +1,13 @@
+import { divide } from "lodash";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>
+            <h1>Laravel x React Configuration Successful</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
